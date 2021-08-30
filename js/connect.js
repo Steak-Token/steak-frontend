e50364dc292b0fd8697e44a57b6ac66e41042f5a
Alkhemeia <admin@alkhemeia.de>
2021-08-30 20:24:26 +0200

    css

--- js/connect.js
+++ js/connect.js
@@ -168,6 +168,7 @@ async function fetchAccountData() {
 
 function isCorrectChain(id) {
     if (id == MAINNET || id == TESTNET) {
+        $(".td-top").css("vertical-align", "top");
         $("#container-header").show();
         $("#screen-connected").show();
         $("#screen-wrong-chain").hide();
